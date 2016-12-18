269224e59c77ecb6b41b8f87da5ef478385a8510
Magdalena Kryś <magdalena.krys0@gmail.com>
2016-12-18 14:10:44 +0100

    getting started - showing db records

---
+++ index.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+    <title>MotoGP world</title>
+    <link rel="icon" type="image/png" href="images/favicon.ico">
+</head>
+<body>
+</body>
+</html>
+
+<!---->
+<?php //echo '<p>Hello World</p>'; ?>
+<?php
+//$serverName = "localhost";
+//$username = "root";
+//$password = "";
+//$dbName = "MotoGP";
+//
+//// Create connection
+//$conn = new mysqli($serverName, $username, $password, $dbName);
+//// Check connection
+//if ($conn->connect_error) {
+//    die("Connection failed: " . $conn->connect_error);
+//}
+//
+//$sql = "SELECT * from motogp.riders";
+//$result = $conn->query($sql);
+//
+//if ($result->num_rows > 0) {
+//    while($row = $result->fetch_assoc()) {
+//        echo "<b>Team: </b>" . $row["Team"]. " - <b>Rider's name: </b>" . $row["Name"]. " - <b>Category:</b> " . $row["Category"]. "<br>";
+//    }
+//} else {
+//    echo "0 results";
+//}
+//$conn->close();
+//?>
